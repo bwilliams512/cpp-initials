67b56096ab83b29de6059838cf65e73f5f6bb1b6
Brian Williams <49660533+bwilliams512@users.noreply.github.com>
2021-12-15 10:03:42 -0600

    Add files via upload

---
+++ initials.cpp
@@ -0,0 +1,15 @@
+// This is a C++ program that displays the initials of my name in block letters
+
+#include <iostream>
+
+int main() {
+
+  std::cout << " BBBB   W   W \n";
+  std::cout << " B   B  W   W \n";
+  std::cout << " B   B  W   W \n";
+  std::cout << " BBBB   W W W \n";
+  std::cout << " B   B  WW WW \n";
+  std::cout << " B   B  WW WW \n";
+  std::cout << " BBBB   W   W \n";
+
+}
